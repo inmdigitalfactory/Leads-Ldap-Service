573cb03d2db4c41ee55ce3f353d226e7074817ea
Emmanuel Zeye <emmanuel.zeye@andela.com>
2022-02-01 13:17:32 +0000

    TD-722: endpoint for token validation and refresh

--- src/main/java/com/imbank/authentication/controllers/AuthController.java
+++ src/main/java/com/imbank/authentication/controllers/AuthController.java
@@ -1,12 +1,24 @@
 package com.imbank.authentication.controllers;
 
 import com.imbank.authentication.dtos.AuthDTO;
+import com.imbank.authentication.dtos.LoginSuccessDTO;
+import com.imbank.authentication.dtos.TokenValidDto;
+import com.imbank.authentication.services.AuthService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("auth")
 public class AuthController {
+
+
+    @Autowired
+    private AuthService authService;
 
     /**
      * This is a fake login api. The actual login logic is handled by spring security. This fake endpoint just documents the login
@@ -17,5 +29,16 @@ public class AuthController {
     @PostMapping("/login")
     public String login(@RequestBody AuthDTO authDto) {
         return "";
+    }
+
+    @RequestMapping("/token/refresh")
+    private ResponseEntity<LoginSuccessDTO> refreshToken() {
+        LoginSuccessDTO loginDto = authService.refreshToken();
+        return ResponseEntity.ok().body(loginDto);
+    }
+
+    @RequestMapping("/token/validate")
+    private ResponseEntity<TokenValidDto> validateToken() {
+        return ResponseEntity.ok().body(new TokenValidDto(authService.isTokenValid()));
     }
 }
